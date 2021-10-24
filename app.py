f112c3ce16292c5342228db520b9b459c9e0d8d2
Andrei <andrei.buchmann@gmail.com>
2021-10-24 21:48:19 +0400

    fix

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/")
-@cross_origin()
 def index():
     return render_template("index.html")
 
